d6e573635aaa2a7619014e6fd0f2e4aab066a2c3
Sergey Kharchenko <sergdevguy@gmail.com>
2019-09-09 13:26:29 +0300

    Add smooth-scroll.js to the main.js file.

--- assets/src/dev_js/main.js
+++ assets/src/dev_js/main.js
@@ -1,7 +1,7 @@
 // BOOTSTRAP 4
 
 // import required js-files
-// ------------------------
+// ---------------------
 //= ../../../node_modules/jquery/dist/jquery.js
 //= ../../../node_modules/bootstrap/js/dist/util.js
 
@@ -10,6 +10,9 @@
 //= ../../../node_modules/bootstrap/js/dist/carousel.js
 
 
+// Import smooth-scroll.js (scroll to id)
+// ---------------------
+//= ../../../node_modules/smooth-scroll/dist/smooth-scroll.js
 
 
 // Import developer js
